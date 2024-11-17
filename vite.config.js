c4b26bb96f4c01dc4b139029557e4b1f90f9cfeb
Murad Lalayev <lalayevmurad@gmail.com>
2024-11-17 19:43:13 +0400

    my commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import { defineConfig } from 'vite';
+
 export default defineConfig({
   base: '/',
   build: {
-    outDir: 'dist',  
-    sourcemap: true,
-  },
+    outDir: 'dist', // Vercel, `dist` klasörünü bekler
+    sourcemap: true  // Debug için opsiyonel
+  }
 });
