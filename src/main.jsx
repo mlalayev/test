5872d59e3b743d46afcee48af671f8de537b240b
Murad Lalayev <lalayevmurad@gmail.com>
2024-11-17 19:51:23 +0400

    my commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';  // React 17 ve sonrasında JSX kullanımı için bu gereklidir
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement); // createRoot ile render işlemi
+root.render(<App />);
