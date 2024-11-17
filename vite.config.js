2dc0946e2545e1a4fbd092015f58c9107579f45b
Murad Lalayev <lalayevmurad@gmail.com>
2024-11-17 19:59:26 +0400

    my comit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  plugins: [react()],
   build: {
-    outDir: 'dist', // Vercel, `dist` klasörünü bekler
-    sourcemap: true  // Debug için opsiyonel
+    outDir: 'dist', // Build dosyaları bu klasöre çıkacak
   }
 });
